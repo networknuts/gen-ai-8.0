174baf699cd4e0e2ae0e119f388c126724e83221
aryan <aryan@networknuts.net>
2026-06-24 03:23:36 +0000

    Added cost tracking

--- mcp/client.py
+++ mcp/client.py
@@ -3,10 +3,21 @@ from dotenv import load_dotenv
 from mcp import ClientSession
 from mcp.client.streamable_http import streamable_http_client
 import asyncio 
+import json 
 
 # SETUP THE ENVIRONMENT
 load_dotenv()
 client = OpenAI()
+
+# SYSTEM PROMPT
+SYSTEM_PROMPT = """
+You are an MCP Client AI assistant with access to external tools
+via MCP. Once you receive the request of the user, check available tools
+and make a decision on whether the user's request should be answered via a tool
+or via your LLM data.
+
+Based upon your decision, continue answering the user's query.
+"""
 
 # DYNAMIC TOOL SCHEMA GENERATION - OPENAI
 def convert_tool_to_schema(tool):
@@ -25,8 +36,34 @@ async def main():
         input_stream
     ):
         async with ClientSession(read_stream, write_stream) as session:
-             await session.initialize()
-             tool_list = await session.list_tools()
-             print(tool_list)
+            await session.initialize()
+            tool_list = await session.list_tools()
+            tools = tool_list.tools
+            openai_tools = []
+            for t in tools:
+                openai_tools.append(convert_tool_to_schema(t))
+            response = client.responses.create(
+                model="gpt-5.4-mini",
+                instructions=SYSTEM_PROMPT,
+                input=query,
+                tools=openai_tools
+            )
+            tool_call = None
+            for item in response.output:
+                if item.type == "function_call":
+                    tool_call = item 
+                    break
+            if tool_call:
+                tool_name = tool_call.name
+                args = json.loads(tool_call.arguments)
+                print(f"LLM SELECTED TOOL: {tool_name}")
+                result = await session.call_tool(tool_name,args)
+                print("\nRUNNING TOOL\n")
+                for item in result.content:
+                    print(item.text)
+            else:
+                print("NO TOOL SELECTED, USING INTERNAL LLM DATA")
+                print(response.output_text)
+        
 
 asyncio.run(main())
